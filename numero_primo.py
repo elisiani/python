cca7d8da028f2b3134ca919692f2bd5f58076273
elisiani <elisianiangelim@gmail.com>
2022-08-23 15:03:10 -0300

    Ajustado o comentário, sem teste unitário.

--- numero_primo.py
+++ numero_primo.py
@@ -1,7 +1,6 @@
 #Verifica se o numero é primo
 #PRIMO - numero primo tem apenas dois divisores - divisivel por 1 e por ele mesmo (não tem resto).
-#Se o resto da divisão for DIFERENTE de zero ENTÃO o numero é primo
-#Se o resto da divisão for IGUAL a zero ENTÂO o numero não é primo
+
 
 print('**********************************\n'
       '********* NUMERO PRIMO ***********\n'
